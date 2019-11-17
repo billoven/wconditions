df01517b898475069357bb25a4340788934b7435
Pierre Stranart <pierre.stranart@free.fr>
2019-11-17 18:38:27 +0100

    SOme WU API information Daily summary URL have been put to start the design.

--- src/MV_UpdateDB.py
+++ src/MV_UpdateDB.py
@@ -9,7 +9,12 @@ print ('Mon premier pas en python')
 # Get date of the day
 
 # Example of Wunderground URL : 'https://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=ILEDEFRA131&day=2&month=12&year=2017&dayend=1&monthend=1&yearend=2018&graphspan=custom&format=1
-# Compose the Wunderground URL
+# Compose the Wunderground URL https://api.weather.com/v2/pws/observations/current?stationId=ILEDEFRA131&format=json&numericPrecision=decimal&units=m&apiKey=43de0fca7f6f49a79e0fca7f6f29a708
+# API WU : https://docs.google.com/document/d/1eKCnKXI9xnoMGRRzOL1xPCBihNV2rOet08qpE_gArAY/edit
+# https://api.weather.com/v2/pws/history/daily?stationId=ILEDEFRA131&format=json&units=m&date=20191101&apiKey=43de0fca7f6f49a79e0fca7f6f29a708&numericPrecision=decimal
+# Once a day a query to get the daily forecast summary via a cron
+# Result are put in a JSON stucture
+# Then the MYSQL Data is updated
 
 # Get csv format result of URL
 
