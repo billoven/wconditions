fda82d98cd44053b0193b0897773a7ec246a9591
Pierre Stranart <Pierre.Stranart@gmail.com>
2023-12-03 18:44:51 +0100

    Navbar modfied to add Theme selection and DB selection

--- weathermetrics/src/weatherMetricsIndex.php
+++ weathermetrics/src/weatherMetricsIndex.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Climatologic Statistics with bootstra5.3.1</title>
-  <link id="bootstrap-theme" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.1/dist/darkly/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+<?php
+    // Common Header for all the weatherMetrics files
+    include "weatherMetricsHeader.php";
 
-    <style>
-        .table th:first-child,
-        .table td:first-child {
-        position: sticky;
-        left: 0;
-        }
-    </style>   
-</head>
-<body class="container mt-5">
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
     <h1 class="mb-4">Select a Weather Site Database</h1>
     <form action="weatherMetrics.php" method="post" class="mx-auto">
         <div class="mb-3 text-center">
