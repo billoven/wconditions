ddfc66f01182332180d7fb0ad0b9fcb0f0e27c4a
Pierre Stranart <Pierre.Stranart@gmail.com>
2024-03-17 17:13:59 +0100

    Adaptation of all selected value in the dropdown-menus

--- weathermetrics/src/weatherMetricsHeader.php
+++ weathermetrics/src/weatherMetricsHeader.php
@@ -39,6 +39,9 @@
             .dropdown-menu > li:hover > .submenu{
                 display: block;
             }
+            .dropdown-submenu .dropdown-menu {
+               width: auto;
+            }
         }	
         /* ============ desktop view .end// ============ */
 
@@ -96,7 +99,7 @@
         echo "SelectedDB received from JavaScript: " . htmlspecialchars($selectedDb) . "<BR>";
 
         // Database configuration
-        require_once('/etc/weathermetrics/db_config.php'); // Adjust the path accordingly
+        require_once('/etc/wconditions/db_config.php'); // Adjust the path accordingly
 
         if (isset($dbConfigs[$selectedDb])) {
             $dbConfig = $dbConfigs[$selectedDb];
@@ -106,12 +109,13 @@
             }
 
             // Echo the host, username, and database
-            echo "Host: " . $dbConfig['host'] . "<br>";
-            echo "Username: " . $dbConfig['username'] . "<br>";
-            echo "Database: " . $dbConfig['database'] . "<br>";
-            echo "TableDwc: " . $dbConfig['tabledwc'] . "<br>";
-            echo "LabelNormals1: " . $dbConfig['LabelNormals1'] . "<br>";
-            echo "LabelNormals2: " . $dbConfig['LabelNormals2'] . "<br>";
+            echo "Weather Station   : " . $dbConfig['weatherStation'] . "<br>";
+            echo "Host              : " . $dbConfig['host'] . "<br>";
+            echo "Username          : " . $dbConfig['username'] . "<br>";
+            echo "Database          : " . $dbConfig['database'] . "<br>";
+            echo "TableDwc          : " . $dbConfig['tabledwc'] . "<br>";
+            echo "DefaultNormals    : " . $dbConfig['DefaultNormals'] . "<br>";
+            echo "DefaultNormalsCity: " . $dbConfig['DefaultNormalsCity'] . "<br>";
     
             // Fetch available years from the database
             $years = array();
@@ -124,6 +128,14 @@
             // Close the database connection
             $conn->close();
         }
+
+        // Check if the selectedNormals variable is set in PHP
+        if (isset($selectedNormals)) {
+            echo $selectedNormals; // Output the selectedNormals value if it's set
+        } else {
+            echo "Select Normals"; // Default text if selectedNormals is not set
+        }
+
     ?>
     <!-- Release Container -->
     <div class="release-container" id="releaseContainer">
@@ -164,52 +176,33 @@
                         <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownNormalsButton" data-bs-toggle="dropdown" aria-expanded="false">
                             Select Normals
                         </button>
-
-                        <!-- Normals selection options -->
+                    
+                        <!-- Normals selection options --> 
                         <ul class="dropdown-menu dropdown-menu-right" id="normals-selector" aria-labelledby="dropdownNormalsButton">
-                            <!-- Weather Station: LilleLesquin -->
-                            <li class="dropdown-submenu">
-                                <a class="dropdown-item dropdown-toggle" href="#">LilleLesquin</a>
-                                <ul class="submenu submenu-left dropdown-menu">
-                                    <!-- Normals Files for LilleLesquin -->
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('LilleLesquin', 'StatsNormals_LIlleLesquin_1951_1980.json')">1951-1980</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('LilleLesquin', 'StatsNormals_LIlleLesquin_1961_1990.json')">1961-1990</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('LilleLesquin', 'StatsNormals_LIlleLesquin_1971_2000.json')">1971-2000</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('LilleLesquin', 'StatsNormals_LIlleLesquin_1981_2010.json')">1981-2010</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('LilleLesquin', 'StatsNormals_LIlleLesquin_1991_2020.json')">1991-2020</a></li>
-                                </ul>
-                            </li>
 
-                            <!-- Weather Station: ParisMontsouris -->
-                            <li class="dropdown-submenu">
-                                <a class="dropdown-item dropdown-toggle" href="#">ParisMontsouris</a>
-                                <ul class="submenu submenu-left dropdown-menu">
-                                    <!-- Normals Files for ParisMontsouris -->
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1873_1900.json')">1873-1900</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1881_1910.json')">1881-1910</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1891_1920.json')">1891-1920</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1901_1930.json')">1901-1930</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1911_1940.json')">1911-1940</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1921_1950.json')">1921-1950</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1931_1960.json')">1931-1960</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1941_1970.json')">1941-1970</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1951_1980.json')">1951-1980</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1961_1990.json')">1961-1990</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1971_2000.json')">1971-2000</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1981_2010.json')">1981-2010</a></li>
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('ParisMontsouris', 'StatsNormals_ParisMontsouris_1991_2020.json')">1991-2020</a></li>
-                                    <!-- Add more as needed -->
-                                </ul>
-                            </li>
+                        <?php
+                            // Read the JSON file
+                            $jsonData = file_get_contents('normals/NormalsFilesList.json');
 
-                            <!-- Weather Station: VillebonSurYvette -->
-                            <li class="dropdown-submenu">
-                                <a class="dropdown-item dropdown-toggle" href="#">VillebonSurYvette</a>
-                                <ul class="submenu submenu-left dropdown-menu">
-                                    <!-- Normals Files for VillebonSurYvette -->
-                                    <li><a class="dropdown-item" href="#" onclick="changeNormals('VillebonSurYvette', 'StatsNormals_VillebonSurYvette_2016_2023.json')">2016-2023</a></li>
-                                </ul>
-                            </li>
+                            // Parse JSON data
+                            $data = json_decode($jsonData, true);
+
+                            // Iterate over WeatherStations
+                            foreach ($data['WeatherStations'] as $weatherStation => $periods) {
+                                // Output the dropdown submenu for each WeatherStation
+                                echo '<li class="dropdown-submenu">';
+                                echo '<a class="dropdown-item dropdown-toggle" href="#">' . $weatherStation . '</a>';
+                                echo '<ul class="submenu submenu-left dropdown-menu">';
+                                
+                                // Output the Normals Files for the WeatherStation
+                                foreach ($periods as $period) {
+                                    echo '<li><a class="dropdown-item" href="#" onclick="changeNormals(\'' . $weatherStation . '\', \'' . $period . '\')">' . str_replace('_', '-', $period) . '</a></li>';
+                                }
+                                
+                                echo '</ul>';
+                                echo '</li>';
+                            }
+                        ?>
                         </ul>
                     </div>
                 </div>
@@ -230,19 +223,18 @@
                                 // Get the selected period from the clicked sub-menu item
                                 const selectedPeriod = $(this).text();
                                 
-                                // Update the text of the "Select Normals" button with the abbreviated city name and period
-                                const abbreviatedCity = selectedCity.substring(0, 2); // Get the first two letters of the city name
-                                const buttonLabel = `${abbreviatedCity}-${selectedPeriod}`; // Concatenate abbreviated city name and period
-                                $('#dropdownNormalsButton').text(buttonLabel);
                                 
-                                // Trigger the changeNormals function with the selected city and period
-                                //changeNormals(selectedCity, selectedPeriod);
-
+                                
                                 // Hide both parent and sub-menu
                                 $(this).closest('.dropdown').removeClass('show').find('.dropdown-menu').removeClass('show');
                                 
+                                // Prevents further propagation of the current event 
+                                // in the capturing and bubbling phases
                                 e.stopPropagation();
+                                // Cancels the event if it is cancelable, meaning that 
+                                // the default action that belongs to the event will not occur
                                 e.preventDefault();
+
                             });
 
 
@@ -251,12 +243,50 @@
                     
                     // Function to change the selected normals
                     function changeNormals(city, selectedPeriod) {
+
+                        console.log("changeNormals function called");
+
+                        // Store the selected Period Json File name in session storage
+                        sessionStorage.setItem('Normals', selectedPeriod);
+                        sessionStorage.setItem('NormalsCity', city);
+                                                                        
                         // Implement logic to handle the selected normals for the specified city
-                        console.log('Changing normals for city:', city);
-                        console.log('Selected period:', selectedPeriod);
+                        // Log the stored selected Normals and City
+                        const storedCity = sessionStorage.getItem('NormalsCity');
+                        console.log("Normals City Stored:", storedCity);
+                        const storedNormals = sessionStorage.getItem('Normals');
+                        console.log("Normals Period Stored:", storedNormals);
+
 
                         // For demonstration purposes, you can perform any action here based on the selected city and period
                         // For example, you can load data from the selected normals file associated with the city
+                        // Function to change the selected normals
+
+                        // Update the text of the "Select Normals" button with the abbreviated city name and period
+                        const abbreviatedCity = city.substring(0, 2); // Get the first two letters of the city name
+                        const buttonLabel = `${abbreviatedCity}-${selectedPeriod}`; // Concatenate abbreviated city name and period
+                        $('#dropdownNormalsButton').text(buttonLabel);
+
+                        // Get the current URL
+                        const currentUrl = window.location.href;
+
+                        // Check if there is already a query string in the URL
+                        const hasQueryString = currentUrl.indexOf('?') !== -1;
+
+                        // Build the new URL with the selectedCity and selectedPeriod as GET parameters
+                        const newUrl = hasQueryString
+                            ? `${currentUrl.split('?')[0]}?selectedCity=${encodeURIComponent(city)}&selectedPeriod=${encodeURIComponent(selectedPeriod)}`
+                            : `${currentUrl}?selectedCity=${encodeURIComponent(city)}&selectedPeriod=${encodeURIComponent(selectedPeriod)}`;
+
+                        // Navigate to the new URL
+                        window.location.href = newUrl;
+
+                        // Log the selected city and period
+                        console.log("Selected city:", city);
+                        console.log("Selected period:", selectedPeriod);
+                        // Log the New Url
+                        console.log("NewUrl:", newUrl);
+                    
                     }
 
                 </script>
@@ -269,9 +299,9 @@
                         </button>
                         <!-- Database selection options -->
                         <ul class="dropdown-menu" id="db-selector" aria-labelledby="dropdownDbButton">
-                            <li><a class="dropdown-item" href="#" onclick="changeDb('db1')">Villebon</a></li>
-                            <li><a class="dropdown-item" href="#" onclick="changeDb('db2')">Bethune</a></li>
-                            <!-- Add more database options as needed -->
+                            <?php foreach ($dbConfigs as $dbid => $dbConfig): ?>
+                            <li><a class="dropdown-item" href="#" onclick="changeDb('<?php echo $dbid; ?>','<?php echo $dbConfig['weatherStation']; ?>')"><?php echo $dbConfig['weatherStation']; ?></a></li>
+                            <?php endforeach; ?>
                         </ul>
                     </div>
                     <div class="dropdown ms-3">
@@ -319,25 +349,11 @@
             changeTheme(selectedTheme);
         });
 
-        // Function to update the text of the dropdown toggle button
-        function updateDropdownButtonText(dbName) {
-            const dropdownButton = document.getElementById('dropdownDbButton');
-            if (dbName === 'db1') {
-                dropdownButton.textContent = 'Villebon';
-            } else if (dbName === 'db2') {
-                dropdownButton.textContent = 'Bethune';
-            } else {
-                dropdownButton.textContent = 'Select DB';
-            }
-        }
-
         // Function to change the selected database
-        function changeDb(dbName) {
+        function changeDb(dbid,station) {
             // Store the selected database in session storage
-            sessionStorage.setItem('selectedDb', dbName);
-
-            // Update the text of the dropdown toggle button
-            updateDropdownButtonText(dbName);
+            sessionStorage.setItem('selectedDb', dbid);
+            sessionStorage.setItem('selectedStation', station);
 
             // Get the current URL
             const currentUrl = window.location.href;
@@ -347,14 +363,16 @@
 
             // Build the new URL with the selectedDb as a GET parameter
             const newUrl = hasQueryString
-                ? `${currentUrl.split('?')[0]}?selectedDb=${encodeURIComponent(dbName)}`
-                : `${currentUrl}?selectedDb=${encodeURIComponent(dbName)}`;
+                ? `${currentUrl.split('?')[0]}?selectedDb=${encodeURIComponent(dbid)}`
+                : `${currentUrl}?selectedDb=${encodeURIComponent(dbid)}`;
 
             // Navigate to the new URL
             window.location.href = newUrl;
 
             // Log the selected database
-            console.log("Selected DB stored:", dbName);
+            console.log("Selected DBid stored:", dbid);
+            console.log("Selected DB station stored:", station);
+            
 
             // Log the New Url
             console.log("NewUrl:", newUrl);
@@ -373,10 +391,18 @@
             });
         });
 
-        // Initial setup: Check if a database is already selected and update the button text
+        // Initial setup: Check if database, normals are already selected 
+        // and update the button text
         document.addEventListener('DOMContentLoaded', function () {
-            const storedDb = sessionStorage.getItem('selectedDb');
-            updateDropdownButtonText(storedDb);
+            const storedStation = sessionStorage.getItem('selectedStation');
+            $('#dropdownDbButton').text(storedStation);
+            
+            const storedNormalsCity = sessionStorage.getItem('NormalsCity');
+            const storedNormalsPeriod = sessionStorage.getItem('Normals');
+            const abbreviatedCity = storedNormalsCity.substring(0, 2); // Get the first two letters of the city name
+            const buttonLabel = `${abbreviatedCity}-${storedNormalsPeriod}`; // Concatenate abbreviated city name and period
+            $('#dropdownNormalsButton').text(buttonLabel);
+            
         });
 
         $(document).ready(function () {
@@ -392,4 +418,3 @@
                 });
         });
     </script>
-
